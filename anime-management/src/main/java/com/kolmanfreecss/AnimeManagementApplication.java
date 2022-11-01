0cd89aeef95af24987d9efb2d33e104550bf9329
Ragnarok <s3rg10.martinez.roman@gmail.com>
2022-11-01 02:15:36 +0100

    - Infraestructure components template added

---
+++ anime-management/src/main/java/com/kolmanfreecss/AnimeManagementApplication.java
@@ -0,0 +1,13 @@
+package com.kolmanfreecss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnimeManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnimeManagementApplication.class, args);
+	}
+
+}
---
+++ anime-management/src/test/java/com/kolmanfreecss/AnimeManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kolmanfreecss;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnimeManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
